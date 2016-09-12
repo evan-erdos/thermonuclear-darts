bdc6468a7e3682fe2e03243fa9203a746c2e6406
Ben Scott <ias.jazz@gmail.com>
2016-09-12 19:43:43 -0400

    basic

---
+++ Assets/Scripts/LawnDart.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof (Rigidbody))]
+class LawnDart : MonoBehaviour {
+
+    bool wait;
+    float length = 0.25f;
+    Transform center;
+
+// disables a silly warning
+#pragma warning disable 0108
+    Rigidbody rigidbody;
+#pragma warning restore 0108
+
+    void Awake() {
+        center = transform.Find("center");
+        rigidbody = GetComponent<Rigidbody>();
+        rigidbody.centerOfMass = center.position;
+    }
+
+
+    // callback for collisions, must have this exact function signature
+    //void OnCollisionEnter(Collision collision) { }
+
+    // called when the front "trigger" collider is tripped
+    void OnTriggerEnter(Collider other) {
+        if (!wait) StartCoroutine(Colliding());
+    }
+
+    IEnumerator Colliding() {
+        wait = true;
+        yield return new WaitForSeconds(rigidbody.velocity.magnitude*length);
+        rigidbody.isKinematic = true;
+        //wait = false;
+    }
+
+}
